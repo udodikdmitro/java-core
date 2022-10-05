76d8ce64056ce2077775451ee7657070ccb6f34d
dima udodik <dima udodik>
2022-10-05 12:21:01 +0300

    Collections, part 3.

--- src/collections/CollectionsRunner.java
+++ src/collections/CollectionsRunner.java
@@ -7,8 +7,8 @@ public class CollectionsRunner {
     public static void main(String[] args) {
         List<Card> decOfCards = new ArrayList<>();
 
-        for (Card.Face face: Card.Face.values()) {
-            for (Card.Suit suit: Card.Suit.values()) {
+        for (Card.Face face : Card.Face.values()) {
+            for (Card.Suit suit : Card.Suit.values()) {
                 decOfCards.add(new Card(suit, face));
             }
         }
@@ -27,7 +27,7 @@ public class CollectionsRunner {
             System.out.printf("%-20s %s", decOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : "  ");
         }
 
-        Collections.sort(decOfCards);
+        Collections.sort(decOfCards, new CardComparator());
 
         System.out.println("\n\nDec of cards after sort:");
 
@@ -38,9 +38,13 @@ public class CollectionsRunner {
 
     public static class Card implements Comparable<Card> {
 
-        private enum Suit {SPADES, HEARTS, CLUBS, DIMONDS};
+        private enum Suit {SPADES, HEARTS, CLUBS, DIMONDS}
 
-        private enum Face {Ace, Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
+        ;
+
+        private enum Face {Ace, Deuce, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King}
+
+        ;
 
         private final Suit suit;
 
@@ -60,12 +64,12 @@ public class CollectionsRunner {
         }
 
         @Override
-        public int compareTo (Card card) {
+        public int compareTo(Card card) {
 
             Face[] values = Face.values();
             List<Face> faces = Arrays.asList(values);
 
-            if(faces.indexOf(this.face) < faces.indexOf(card.getFace())) {
+            if (faces.indexOf(this.face) < faces.indexOf(card.getFace())) {
                 return -1;
             } else if (faces.indexOf(this.face) > faces.indexOf(card.getFace())) {
                 return +1;
@@ -76,8 +80,25 @@ public class CollectionsRunner {
         }
 
         @Override
-        public String toString () {
-            return face+" of "+suit;
+        public String toString() {
+            return face + " of " + suit;
+        }
+    }
+
+    public static class CardComparator implements Comparator<Card> {
+        List<Card.Face> faces = Arrays.asList(Card.Face.values());
+
+        @Override
+        public int compare(Card card1, Card card2) {
+
+            if (faces.indexOf(card1.getFace()) < faces.indexOf(card2.getFace())) {
+                return 1;
+            } else if (faces.indexOf(card1.getFace()) > faces.indexOf(card2.getFace())) {
+                return -1;
+            } else if (faces.indexOf(card1.getFace()) == faces.indexOf(card2.getFace())) {
+                return String.valueOf(card1.suit).compareTo(String.valueOf(card2.getSuit()));
+            }
+            return 0;
         }
     }
 }
