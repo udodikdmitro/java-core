f4979e005a1e2a2b4b9a92221809b6087ddd2e69
dima udodik <dima udodik>
2023-01-04 07:10:06 +0200

    Collections. Map. Part 1.

--- src/map/MapLauncher.java
+++ src/map/MapLauncher.java
@@ -1,14 +1,34 @@
 package map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MapLauncher {
 
     public static void main(String[] args) {
-        Map<String, Integer> numbers = new HashMap<>();
-        numbers.put("Toyota",50);
-        numbers.put("Audi",51);
-        System.out.println(numbers.put("Toyota",52));
+        Map<String, Integer> wordMap = new HashMap<>();
+        System.out.println("Please enter some text: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        String[] tokens = string.split(" ");
+
+        for(String token: tokens){
+            String word = token.toLowerCase();
+            Integer count = wordMap.get(word);
+            if(count == null){
+                wordMap.put(word,1);
+            } else {
+                wordMap.put(word,count + 1);
+            }
+        }
+        printMap(wordMap);
+    }
+
+    private static void printMap(Map<String, Integer> wordMap) {
+        Map<String, Integer> wordTreeMap = new TreeMap<>(wordMap);
+        Set<String> keys = wordTreeMap.keySet();
+
+        for(String key: keys){
+//            System.out.println("%-10s%-10s \n", key, wordMap.get(key));
+        }
     }
 }
