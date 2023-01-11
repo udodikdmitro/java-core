139ae60e0659ed64418af056dd96fea12061d65b
dima udodik <dima udodik>
2023-01-11 12:14:39 +0200

    Reader and writer. Part 1.

---
+++ src/io/IOMain.java
@@ -0,0 +1,41 @@
+package io;
+
+import map.treeMap.AverageStudentGrade;
+import map.treeMap.SubjectGrade;
+import map.treeMap.TreeMapRunner;
+
+import java.io.*;
+import java.util.NavigableMap;
+import java.util.Set;
+
+public class IOMain {
+
+    public static final String FILE_NAME = "GradeBook.txt";
+
+    public static void main(String[] args) throws IOException {
+        NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
+        readFile(grades);
+        readFile();
+    }
+
+    private static void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+        String c;
+        while ((c = reader.readLine()) != null){
+            System.out.println(c);
+        }
+    }
+
+    private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))){
+            for (AverageStudentGrade gradeKey : grades.keySet()) {
+                writer.write("------------------------------------\n");
+                writer.write("Student: " + gradeKey.getName() + " Average grade: " +
+                        gradeKey.getAverageGrade() + "\n");
+                for (SubjectGrade grade : grades.get(gradeKey)) {
+                    writer.write("Subject: " + grade.getSubject() + " Grade: " + grade.getGrade() + "\n");
+                }
+            }
+        }
+    }
+}
