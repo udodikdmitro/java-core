4ade4063c1430be8620e844f155688af97e63511
dima udodik <dima udodik>
2023-04-02 08:38:10 +0300

    Lambda, higher order functions.

---
+++ src/lambdas/RichPerson.java
@@ -0,0 +1,46 @@
+package lambdas;
+
+public class RichPerson {
+
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private int age;
+
+    public RichPerson(String firstName, String lastName, int salary, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "RichPerson{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
